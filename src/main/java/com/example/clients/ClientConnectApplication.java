0b6fed4ede185acf226dd7f6eaf21298adbf236d
maria <mariacodes77@gmail.com>
2025-02-04 00:28:03 +0300

    chore: initialize Spring Boot project with dependencies and configuration

---
+++ src/main/java/com/example/clients/ClientConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConnectApplication.class, args);
+	}
+
+}
