e92f83c3630f7a21ec78d1046885f6803b8fcbcd
maria <mariacodes77@gmail.com>
2025-02-04 01:22:50 +0300

    feat: add repositories for Client and Contact

---
+++ src/main/java/com/example/clients/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.example.clients.repository;
+
+import com.example.clients.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
