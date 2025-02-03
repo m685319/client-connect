335e9bf25356299ffa3329a2186bdf042796a2d0
maria <mariacodes77@gmail.com>
2025-02-04 00:57:24 +0300

    feat: add Client and Contact entities

---
+++ src/main/java/com/example/clients/model/Client.java
@@ -0,0 +1,24 @@
+package com.example.clients.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    private String name;
+    private String lastName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    private Contact contact;
+}
---
+++ src/main/java/com/example/clients/model/Contact.java
@@ -0,0 +1,20 @@
+package com.example.clients.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String phone;
+    private String email;
+}
