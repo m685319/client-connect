dc0e8927345caef9e902e071bae326f9f547203d
maria <mariacodes77@gmail.com>
2025-02-05 04:25:33 +0300

    feat: add global exception handling and Swagger configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO client (name, last_name) VALUES ('John', 'Doe');
+INSERT INTO contact (phone, email, client_id) VALUES ('+1234567890', 'john@example.com', 1);
+
+INSERT INTO client (name, last_name) VALUES ('Mike', 'Hitman');
+INSERT INTO contact (phone, email, client_id) VALUES ('+9234567890', 'mike@example.com', 2);
